57e9961b7e30500a9d874814917bc138573cc49d
hide0325 <tmnftc@yahoo.co.jp>
2023-09-18 12:11:38 +0900

    finished PART1

---
+++ Top-Restaurants/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+        },
+      ],
+    },
+  },
+});
